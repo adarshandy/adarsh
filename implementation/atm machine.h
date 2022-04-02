de9efb119c36ee183dbffc19be52cbf7ee3b7eb3
adarshandy <102019382+adarshandy@users.noreply.github.com>
2022-04-02 17:06:12 +0530

    Add files via upload

---
+++ implementation/atm machine.h
@@ -0,0 +1,13 @@
+
+
+#ifndef __ATM_H__
+#define __ATM_H__
+
+void mainMenu(void);
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit(void);
+void errorMessage(void);
+
+#endif
